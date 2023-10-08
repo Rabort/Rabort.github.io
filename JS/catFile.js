07e0d94f531ee749b5f76fe9fcb377490b8abdfb
lolmak <lolmakGS@ya.ru>
2023-10-08 18:22:33 +0700

    Отображение загружаемой картинки
    • Загружаемая на сайт картинка теперь отображается снизу под кнопкой. Размер картинки зависит от размера.

--- JS/catFile.js
+++ JS/catFile.js
@@ -15,11 +15,27 @@ function catUpload() {
 
 var input = document.getElementById('catFile');
 input.addEventListener('change', () => {
-	swal({
-		title: "lolmak",
-		text: "Короче, кнопка теперь работает! " +
-		"но я в душе не бум бум что она должна делать...",
-		icon: "info",
-		button: "пофиг!",
-	});
-});
+	const catFile = document.getElementById('catFile');
+	const file = catFile.files[0];
+	if (!file) {
+		console.warn('Загружаемый файл не был выбран. Отменяем операцию.');
+		return
+	}
+	const reader = new FileReader();
+	reader.readAsDataURL(file);
+	reader.onload = function (event) {
+		document.getElementById("imgCat").style.height = '';
+		document.getElementById("imgCat").style.width = '';
+		document.getElementById("imgCat").src = event.target.result;
+		const imgCat = new Image();
+		imgCat.src = event.target.result;
+		imgCat.onload = () => {
+			if (imgCat.height > 300) {
+				document.getElementById("imgCat").style.height = '300px'
+			};
+			if (imgCat.width > 450) {
+				document.getElementById("imgCat").style.width = '450px'
+			}
+		}
+	}
+})
